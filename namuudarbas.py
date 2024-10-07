9279126c8f53424c45a494361acede8f7c86da2d
Ttenel <145286079+Ttenel@users.noreply.github.com>
2024-10-07 19:08:42 +0300

    namuudarbas.py

--- namuudarbas.py
+++ namuudarbas.py
@@ -13,8 +13,8 @@ def b(n):
 
 start_time = time.time()
 b(my_list)
-print(f"Bubble sort: {time.time() - start_time:.10f} s")
+print(f"Bubble sort algoritmas: {time.time() - start_time:.10f} s")
 
 start_time = time.time()
 sorted(my_list)
-print(f"Built-in sort: {time.time() - start_time:.10f} s")
+print(f"Sorted funkcija: {time.time() - start_time:.10f} s")
